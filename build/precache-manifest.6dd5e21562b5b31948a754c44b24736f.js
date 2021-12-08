42b45e3e533a111db209c4c2250db73a1365e46a
Rafael Romano <rafa-kun@hotmail.com>
2021-12-08 10:36:34 -0300

    :zap: testando
    vqv

---
+++ build/precache-manifest.6dd5e21562b5b31948a754c44b24736f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1ef9d308dce84762b579455d89e3a5",
+    "url": "/recipe-app-react/index.html"
+  },
+  {
+    "revision": "6e253f96dfe88a344381",
+    "url": "/recipe-app-react/static/css/main.c38cc26c.chunk.css"
+  },
+  {
+    "revision": "94cae259740146dbd6a5",
+    "url": "/recipe-app-react/static/js/2.596ece37.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipe-app-react/static/js/2.596ece37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e253f96dfe88a344381",
+    "url": "/recipe-app-react/static/js/main.0ca0a86b.chunk.js"
+  },
+  {
+    "revision": "309a2fc531f3a866d350",
+    "url": "/recipe-app-react/static/js/runtime-main.6d258d38.js"
+  },
+  {
+    "revision": "5c7eaa4ad858ebc749b1794f708b2400",
+    "url": "/recipe-app-react/static/media/blackHeartIcon.5c7eaa4a.svg"
+  },
+  {
+    "revision": "b2486c57b81c606ea93e4a736286956d",
+    "url": "/recipe-app-react/static/media/drinkIcon.b2486c57.svg"
+  },
+  {
+    "revision": "cf18b656f3b9f7eb9625bc32796a8238",
+    "url": "/recipe-app-react/static/media/exploreIcon.cf18b656.svg"
+  },
+  {
+    "revision": "b9a4279af580cbad6d564beadcd47d40",
+    "url": "/recipe-app-react/static/media/globo.b9a4279a.png"
+  },
+  {
+    "revision": "9bebca47762980aeab05287a0d0b987c",
+    "url": "/recipe-app-react/static/media/ingredientes.9bebca47.png"
+  },
+  {
+    "revision": "c924d70469a9006ac828bc5becb57ed3",
+    "url": "/recipe-app-react/static/media/logo-ver.c924d704.png"
+  },
+  {
+    "revision": "7f0525f63560e30d5a8101b6459b81e5",
+    "url": "/recipe-app-react/static/media/logo_lr.7f0525f6.gif"
+  },
+  {
+    "revision": "c59a398ed07b0662e8a9d662614ce4e1",
+    "url": "/recipe-app-react/static/media/mealIcon.c59a398e.svg"
+  },
+  {
+    "revision": "90b8c9393e902346165e51f78de740d3",
+    "url": "/recipe-app-react/static/media/profileIcon.90b8c939.svg"
+  },
+  {
+    "revision": "25708205eb3195ecfe845c5adafcff1c",
+    "url": "/recipe-app-react/static/media/searchIcon.25708205.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipe-app-react/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipe-app-react/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
